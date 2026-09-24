394ac7affbdbbb87a6d38fe46aa7a07ee311f8e7
Muhammad Adil <adiillmuhammad7@gmail.com>
2026-09-24 15:29:39 +0530

    Time: 0 ms (100%), Space: 53.8 MB (89.55%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+
+    let sum = 0
+    let result = []
+
+    for(let i = 0; i < nums.length; i++) {
+
+        sum = sum + nums[i]
+        result.push(sum)
+
+    }
+    return result
+
+};
