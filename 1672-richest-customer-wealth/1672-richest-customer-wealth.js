c4cc08c553f1511b9cf156c803d2314e87974663
Muhammad Adil <adiillmuhammad7@gmail.com>
2026-09-25 10:05:42 +0530

    Time: 0 ms (100%), Space: 53.7 MB (68.92%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+
+    let max = 0
+
+    for (let i = 0; i < accounts.length; i++) {
+
+        let sum = 0
+
+        for (let j = 0; j < accounts[i].length; j++) {
+            sum = sum + accounts[i][j]
+        }
+
+        if (sum > max) {
+            max = sum
+        }
+    }
+
+    return max
+};
